0817130642ecb101a9e66476c6d5d20a3952995d
Adam Schubert <adamgschubert@gmail.com>
2023-06-07 10:40:54 -0500

    added classes

---
+++ Muscle.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace heist
+{
+    public class Muscle : IRobber
+    {
+        public string Name { get ; set ; }
+        public int SkillLevel { get ; set ; }
+        public int PercentageCut { get ; set ; }
+
+        public void PerformSkill(Bank bank)
+        {
+            bank.SecurityGuardScore =- SkillLevel;
+            
+            if(bank.SecurityGuardScore <=0){
+                Console.WriteLine($"{Name} 'wiffled' the guard");
+            }else{
+                Console.WriteLine($"{Name} has injured the guard by {SkillLevel} points");
+            }
+        }
+    }
+}
